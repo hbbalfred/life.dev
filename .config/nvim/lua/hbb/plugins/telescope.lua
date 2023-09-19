9aab4b681811efd565e1ceadf5d5fbc5969ad2c0
chenran <ran.chen@yo-star.com>
2023-09-19 11:52:45 +0800

    feat: use telescope file browser instead of nvim-tree

--- .config/nvim/lua/hbb/plugins/telescope.lua
+++ .config/nvim/lua/hbb/plugins/telescope.lua
@@ -4,31 +4,45 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-file-browser.nvim",
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local fb_actions = telescope.extensions.file_browser.actions
 
     telescope.setup({
       defaults = {
         path_display = { "truncate " },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.close,
+            ['<C-k>'] = actions.move_selection_previous, -- move to prev result
+            ['<C-j>'] = actions.move_selection_next, -- move to next result
+            ['<C-q>'] = actions.close,
             ['<C-h>'] = actions.select_horizontal,
             ['<C-x>'] = actions.delete_buffer,
+            ['<C-n>'] = fb_actions.create,
+            ['<C-d>'] = fb_actions.remove,
+            ['<C-r>'] = fb_actions.rename,
           },
           n = {
             ['q'] = actions.close
           }
         },
       },
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          grouped = true,
+          respect_gitignore = false,
+          hidden = true,
+        }
+      }
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("file_browser")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
@@ -40,5 +54,8 @@ return {
     keymap.set('n', '<leader>ss', '<cmd>Telescope grep_string<CR>')
     keymap.set('n', '<leader>sh', '<cmd>Telescope help_tags<CR>')
     keymap.set('n', '<leader>sd', '<cmd>Telescope diagnostics<CR>')
+
+    -- set keymaps for file browser
+    keymap.set('n', '<leader>e', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>')
   end,
 }
