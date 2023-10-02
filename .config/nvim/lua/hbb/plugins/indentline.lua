fce1eba896b6f4ae367a01c256884f42436cb0df
hbb <hbbalfred@gmail.com>
2023-10-02 12:28:44 +0800

    fix: migrate indent-blankline to version 3

--- .config/nvim/lua/hbb/plugins/indentline.lua
+++ .config/nvim/lua/hbb/plugins/indentline.lua
@@ -1,27 +1,14 @@
 return {
   'lukas-reineke/indent-blankline.nvim',-- Add indentation guides even on blank lines
+  main = "ibl",
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
 
-    local indentline = require('indent_blankline')
-    indentline.setup({
-      -- char = '|',
-      -- char = '¦',
-      -- char = '┆',
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      show_end_of_line = true,
-
-      -- need nvim-treesitter for context
-      show_current_context = true,
-      show_current_context_start = true,
+    local ibl = require('ibl')
+    ibl.setup({
+      indent = { char = '┊' }
     })
 
-    -- Disable colors of this plugin to prevent weird displaying
-    vim.cmd('highlight IndentBlanklineChar guifg=#928374 gui=nocombine')
-    vim.cmd('highlight IndentBlanklineSpaceChar guifg=NONE gui=nocombine')
-    vim.cmd('highlight IndentBlanklineSpaceCharBlankline guifg=NONE gui=nocombine')
-    vim.cmd('highlight IndentBlanklineContextChar guifg=#928374 gui=nocombine')
   end
 }
 
