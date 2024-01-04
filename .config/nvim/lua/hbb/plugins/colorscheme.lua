72bb9f543cf441532da10f9ef53b586d7ac2d1c4
hbb <hbbalfred@gmail.com>
2024-01-04 23:04:58 +0800

    chore: colorschema

--- .config/nvim/lua/hbb/plugins/colorscheme.lua
+++ .config/nvim/lua/hbb/plugins/colorscheme.lua
@@ -4,10 +4,12 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('gruvbox').setup({
-        transparent_mode = true
+        transparent_mode = false,
+        contrast = "soft"
       })
+
       -- load the colorscheme here
-      vim.cmd([[colorscheme gruvbox]])
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 }
