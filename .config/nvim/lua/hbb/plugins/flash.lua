7fc1b792c775baf54729014f87921deed72f674f
chenran <ran.chen@yo-star.com>
2024-01-19 14:38:54 +0800

    feat: use flash instead of leap

---
+++ .config/nvim/lua/hbb/plugins/flash.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  opts = {
+    modes = {
+      char = {
+        enabled = false
+      }
+    }
+  },
+  keys = {
+    { "<leader>fl", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>ft", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<leader>fs", mode = { "n", "x", "o" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  }
+}
