cac84a30640dcbfd5a6e2e4640f64239546c52f0
hbb <hbbalfred@gmail.com>
2024-02-01 21:28:52 +0800

    fix: rust.vim

--- .config/nvim/lua/hbb/core/options.lua
+++ .config/nvim/lua/hbb/core/options.lua
@@ -60,10 +60,3 @@ vim.api.nvim_create_autocmd('InsertLeave', {
   pattern = '*',
   command = 'set nopaste'
 })
--- AutoSave
-vim.api.nvim_create_autocmd('BufWritePre', {
-  buffer = vim.api.nvim_get_current_buf(),
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end
-})
--- .config/nvim/lua/hbb/plugins/rustaceanvim.lua
+++ .config/nvim/lua/hbb/plugins/rustaceanvim.lua
@@ -2,10 +2,22 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^4",
   ft = { "rust" },
+  dependencies = {
+    "rust-lang/rust.vim"
+  },
   init = function()
+    vim.g.rustfmt_autosave = 1
+
     vim.g.rustaceanvim = {
       server = {
         on_attach = require("hbb.utils.lsp").on_attach,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true
+            }
+          }
+        }
       },
     }
   end,
