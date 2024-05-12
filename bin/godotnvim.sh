4963d1c7d023202b76d093c92981d20a05eb5743
hbb <hbbalfred@gmail.com>
2024-05-12 16:40:27 +0800

    fix: godotnvim cwd

--- bin/godotnvim.sh
+++ bin/godotnvim.sh
@@ -11,15 +11,15 @@ nvim_exec="nvim"
 server_path="$HOME/.cache/nvim/godot-server.pipe"
 
 start_server() {
-    "$term_exec" -e "$nvim_exec" --listen "$server_path" "$1"
+  "$term_exec" -o "working_directory=\"$(pwd)\"" -e "$nvim_exec" --listen "$server_path" "$1"
 }
 
 open_file_in_server() {
-    "$term_exec" -e "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:n $1<CR>:call cursor($2)<CR>"
+  "$term_exec" -o "working_directory=\"$(pwd)\"" -e "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:n $1<CR>:call cursor($2)<CR>"
 }
 
 if ! [ -e "$server_path" ]; then
-    start_server "$1"
+  start_server "$1"
 else 
-    open_file_in_server "$1" "$2"
+  open_file_in_server "$1" "$2"
 fi
