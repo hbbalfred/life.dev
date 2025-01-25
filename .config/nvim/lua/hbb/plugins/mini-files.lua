59a3435c726e39f2b329d81b781343d27f5724e4
hbb <hbbalfred@gmail.com>
2025-01-25 08:57:14 +0800

    feat: open mini.files in cwd

---
+++ .config/nvim/lua/hbb/plugins/mini-files.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.files",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  keys = {
+    {
+      "<leader>o",
+      function()
+        local mf = require('mini.files')
+        mf.open(vim.api.nvim_buf_get_name(0), false)
+        mf.reveal_cwd()
+      end,
+      desc = "Mini files"
+    }
+  },
+  opts = {
+    windows = {
+      preview = true,
+      width_preview = 80
+    }
+  }
+}
--- .config/nvim/lua/hbb/plugins/mini.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "echasnovski/mini.files",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  keys = {
-    { "<leader>o", function() require('mini.files').open() end, desc = "Mini files" }
-  }
-}
