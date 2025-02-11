cc2036e2795faa12b00b9b0a058294d56a668ce4
hbb <hbbalfred@gmail.com>
2025-02-11 17:29:12 +0800

    feat: add conform formatter

---
+++ .config/nvim/lua/hbb/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				json = { "biome" },
+				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				csharp = { "csharpier" },
+			},
+		})
+	end,
+}
