69cea0c8ebb58857362bb4854c42622265027f10
hbb <hbbalfred@gmail.com>
2025-03-10 11:31:42 +0800

    refactor lsp config

--- config/nvim/lua/hbb/plugins/lspconfig.lua
+++ config/nvim/lua/hbb/plugins/lspconfig.lua
@@ -2,53 +2,43 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
+		"saghen/blink.cmp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
-	config = function()
+	config = function(_, opts)
 		local lspconfig = require("lspconfig")
 
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-		local on_attach = require("hbb.utils.lsp").on_attach
-
-		-- configure lua server (with special settings)
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
+		for server, config in pairs(opts.servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			config.on_attach = require("hbb.utils.lsp").on_attach
+			lspconfig[server].setup(config)
+		end
+	end,
+	opts = {
+		servers = {
+			lua_ls = {
+				settings = { -- custom settings for lua
+					Lua = {
+						-- make the language server recognize "vim" global
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							-- make language server aware of runtime files
+							library = {
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.stdpath("config") .. "/lua"] = true,
+							},
 						},
 					},
 				},
 			},
-		})
-
-		-- configure json for formatting
-		lspconfig.jsonls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			init_options = {
-				provideFormatter = true,
+			jsonls = {
+				init_options = {
+					provideFormatter = true,
+				},
 			},
-		})
-
-		-- @dependency {https://github.com/razzmatazz/csharp-language-server}
-		-- @dependency {export DOTNET_ROOT & export DOTNET_TOOLS}
-		lspconfig.csharp_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end,
+			csharp_ls = {},
+		},
+	},
 }
