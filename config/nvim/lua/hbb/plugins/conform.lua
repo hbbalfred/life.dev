01f2e974913b8dc7c198952f4d020352624bca41
hbb <hbbalfred@gmail.com>
2025-03-27 10:46:25 +0800

    supports python

--- config/nvim/lua/hbb/plugins/conform.lua
+++ config/nvim/lua/hbb/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
 				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
 				typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
 				cs = { "csharpier", timeout_ms = 10000 },
+				python = { "isort", "black", timeout_ms = 10000 },
 			},
 		})
 	end,
--- config/nvim/lua/hbb/plugins/lspconfig.lua
+++ config/nvim/lua/hbb/plugins/lspconfig.lua
@@ -39,6 +39,7 @@ return {
 				},
 			},
 			csharp_ls = {},
+      pylsp = {},
 		},
 	},
 }
