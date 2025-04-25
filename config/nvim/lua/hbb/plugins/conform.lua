94bdbe45f4f887dea73f0f6220cd12778a2ae7eb
hbb <hbbalfred@gmail.com>
2025-04-25 14:38:56 +0800

    config conform manually

--- config/nvim/lua/hbb/plugins/conform.lua
+++ config/nvim/lua/hbb/plugins/conform.lua
@@ -1,16 +1,21 @@
 return {
 	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				rust = { "rustfmt", lsp_format = "fallback" },
-				json = { "biome" },
-				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
-				typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
-				cs = { "csharpier", timeout_ms = 10000 },
-				python = { "isort", "black", timeout_ms = 10000 },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			json = { "biome" },
+			javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+			typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+			cs = { "csharpier", timeout_ms = 10000 },
+			python = { "isort", "black", timeout_ms = 10000 },
+		},
+		-- log_level = vim.log.levels.DEBUG,
+		formatters = {
+			csharpier = {
+				command = "csharpier",
+				args = { "format" },
 			},
-		})
-	end,
+		},
+	},
 }
