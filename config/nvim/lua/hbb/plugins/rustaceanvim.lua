a997f11086be5fb37655addc8e1bafef3cd1493c
hbb <hbbalfred@gmail.com>
2025-05-11 22:04:03 +0800

    update rustaceanvim

--- config/nvim/lua/hbb/plugins/rustaceanvim.lua
+++ config/nvim/lua/hbb/plugins/rustaceanvim.lua
@@ -1,13 +1,9 @@
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^4",
-	ft = { "rust" },
-	dependencies = {
-		"rust-lang/rust.vim",
-	},
+	version = "^6",
+	lazy = false,
 	init = function()
 		vim.g.rustfmt_autosave = 1
-
 		vim.g.rustaceanvim = {
 			server = {
 				on_attach = require("hbb.utils.lsp").on_attach,
