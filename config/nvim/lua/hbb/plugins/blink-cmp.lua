5439e94255dd16026a1c155d3184cfd91aec64c1
hbb <hbbalfred@gmail.com>
2025-05-17 20:29:55 +0800

    change blink config

--- config/nvim/lua/hbb/plugins/blink-cmp.lua
+++ config/nvim/lua/hbb/plugins/blink-cmp.lua
@@ -47,7 +47,8 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets" },
+			min_keyword_length = 2,
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
