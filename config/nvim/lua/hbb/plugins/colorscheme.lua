62368b7ed0987a7010a6395d3385a55874cb9474
hbb <hbbalfred@gmail.com>
2025-10-31 22:11:19 +0800

    modify todo

--- config/nvim/lua/hbb/plugins/colorscheme.lua
+++ config/nvim/lua/hbb/plugins/colorscheme.lua
@@ -3,8 +3,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         require("gruvbox").setup({
-            transparent_mode = false,
-            contrast = "hard",
+            contrast = "hard"
         })
 
         -- load the colorscheme here
